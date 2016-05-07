7608d7635491127f4984181511b00c6bf11a8853
Hacer <hacertilbec@std.sehir.edu.tr>
2016-05-07 15:24:33 +0300

    LDA - Spark

--- basic/codes/SimpleApp.py
+++ basic/codes/SimpleApp.py
@@ -2,82 +2,53 @@ from pyspark import SparkContext
 import sys
 from pyspark.mllib.clustering import LDA, LDAModel
 from pyspark.mllib.linalg import Vectors
-from pyspark.sql import Row
+from pyspark.sql import SQLContext,Row
 import os
+from pyspark.ml.feature import CountVectorizer
 #!/usr/bin/env python
 #-*-coding:utf-8-*-
-
-def func(x):
-	result=[]
-	index = x[0]
-	for i in x[1]:
-		result.append(((index, i),1))
-	return result
-def ok(v):
-	k = ""
-	for i in v:
-		k = k + str(i[1]) + " "
-	return k
-def f(x,y):
-	if y==None:
-		return x
-	else:
-		return x+y
-def d(x,numberoftweets):
-	result=[]
-	for i in range(numberoftweets):
-		a= ((i,x),0)
-		result.append(a)
-	return result
-
+def topic_render(topic,wordNumbers,vocabArray):
+    terms = topic[0]
+    termWeights = topic[1]
+    result = []
+    for i in range(wordNumbers):
+        term = vocabArray[terms[i]]
+        result.append((term,termWeights[i]))
+    return result
 def main():
 	p = sys.argv[1]
 	logFile = "data/" + p + "_cleaned.txt"
 	sc = SparkContext("local", "simpleApp")
-	data = sc.textFile(logFile).cache()
-	numberoftweets = data.count()
-	words = data.flatMap(lambda x: x.split(" ")).distinct()
-	word_list = words.collect()
-	words = words.flatMap(lambda x:d(x,numberoftweets))
-	data = data.zipWithIndex().map(lambda (x,y): (y,x.split(" ")))
-	wc = data.flatMap(lambda x: func(x)).groupByKey().mapValues(lambda x: len(x))
-	mat = words.leftOuterJoin(wc).map(lambda (x,y):  (x[0],(x[1], f(y[0],y[1])))).groupByKey().sortByKey().mapValues(lambda x:list(x)).mapValues(lambda x: ok(x))
-	parsedData = mat.mapValues(lambda line: Vectors.dense([float(x) for x in line.strip().split(' ')])).map(lambda (x,y): [x,y])
-
-	# Index documents with unique IDs
-	corpus = parsedData.cache()
-
+	sqlContext = SQLContext(sc)
+	data = sc.textFile(logFile).zipWithIndex().map(lambda (words,idd): Row(idd= idd, words = words.split(" "))).cache()
+	docDF = sqlContext.createDataFrame(data)
+	Vector = CountVectorizer(inputCol="words", outputCol="vectors")
+	model = Vector.fit(docDF)
+	result = model.transform(docDF)
+	corpus_size = result.count()
+	
+	corpus = result.select("idd", "vectors").map(lambda (x,y): [x,y]).cache()
+	
 	# Cluster the documents into three topics using LDA
-	ldaModel = LDA.train(corpus, k=3)
-
-	# Output topics. Each is a distribution over words (matching word count vectors)
+	ldaModel = LDA.train(corpus, k=3,maxIterations=100,optimizer='online')
 	topics = ldaModel.topicsMatrix()
-
-	# print(topics)
-
-	topics_dict={}
-	for topic in range(3):
-		k = "Topic "+ str(topic)
-		topics_dict[k] = {}
-		for word in range(0, ldaModel.vocabSize()):
-			topics_dict[k][str(topics[word][topic])] = word_list[word]
+	wordNumbers = 10
+	topicIndices = sc.parallelize(ldaModel.describeTopics(maxTermsPerTopic = wordNumbers))
+	vocabArray = model.vocabulary
+	topics_final = topicIndices.map(lambda topic: topic_render(topic,wordNumbers,vocabArray)).collect()
 
 	path = "data/" + p + "_results.txt"
 	json = open(path, 'wb')
 	json.close()
 
-	for i in topics_dict.keys():
-		counter=0
-		z = sorted(topics_dict[i],reverse=True)
-		for l in z:
-			if counter == 7: break
-			line = topics_dict[i][l] + " "
+	for topic in topics_final:
+		for term in topic:
+			line = term + " "
 
 			try:
 				string_for_output = line.encode('utf8', 'replace')
 				if string_for_output != " ":
 					os.system("python3 basic/codes/p3p.py " +  string_for_output + "  >> " + path)
-					counter += 1
 			except: pass
 
 		os.system("python3 basic/codes/p3p.py " +  "delmch" + "  >> " + path)
